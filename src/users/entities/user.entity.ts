a44576a5f5860ad7bcd085023997104979bdffbe
Aleksandr Petrov <petrov.aleksr@gmail.com>
2025-07-01 23:55:20 +0500

    update user.entity.ts

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -40,52 +40,82 @@ export class User {
   @IsNotEmpty()
   @IsEmail()
   @Length(6, 255)
-  @Column({ name: 'email', unique: true, length: 255, select: false })
+  @Column({
+    name: 'email',
+    unique: true,
+    select: false,
+    length: 255,
+  })
   email!: string;
-
-  @IsNotEmpty()
-  @IsString()
-  @Length(8, 100)
-  @Column({ name: 'password', length: 100, select: false })
-  password!: string;
-
-  @IsOptional()
-  @IsString()
-  @Length(2, 50)
-  @Column({ name: 'nickname', nullable: true, unique: true, length: 50 })
-  nickname?: string;
 
   @IsOptional()
   @IsPhoneNumber()
   @Length(4, 30)
   @Column({
     name: 'phone_number',
+    unique: true,
     nullable: true,
-    length: 20,
+    select: false,
+    length: 30,
+  })
+  phone_number?: string | null;
+
+  @IsOptional()
+  @IsString()
+  @Length(2, 50)
+  @Column({
+    name: 'nickname',
+    unique: true,
+    nullable: true,
+    length: 50,
+  })
+  nickname?: string | null;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(8, 100)
+  @Column({
+    name: 'password',
+    length: 100,
     select: false,
   })
-  phone_number?: string;
+  password!: string;
 
   @IsOptional()
   @IsInt()
   @Length(0, 200)
-  @Column({ name: 'age', nullable: true, type: 'smallint' })
-  age?: number;
+  @Column({
+    type: 'smallint',
+    name: 'age',
+    nullable: true,
+    length: 200,
+  })
+  age?: number | null;
 
   @IsOptional()
   @IsString()
-  @Column({ name: 'gender', nullable: true, length: 20 })
-  gender?: string;
+  @Length(0, 20)
+  @Column({
+    name: 'gender',
+    nullable: true,
+    length: 20,
+  })
+  gender?: string | null;
 
   @IsOptional()
   @IsString()
   @Length(0, 1000)
-  @Column({ name: 'about', nullable: true, type: 'text' })
-  about?: string;
+  @Column({
+    type: 'text',
+    name: 'about',
+    nullable: true,
+    length: 1000,
+  })
+  about?: string | null;
 
   @Column({
-    name: 'refresh_token',
     type: 'varchar',
+    name: 'refresh_token',
     length: 512,
     nullable: true,
     select: false,
