daec3e914c760b4d8b9bfa728a9e6ad826ee7a16
Aleksandr Petrov <37398682+aleksr777@users.noreply.github.com>
2026-09-08 23:40:16 +0500

    Add administrator transfer cancellation errors

--- src/common/errors-service/error-messages.type.ts
+++ src/common/errors-service/error-messages.type.ts
@@ -50,6 +50,9 @@ export enum ErrMsg {
 
   ONLY_ADMINISTRATOR_TRANSFER = 'Only administrator can initiate a transfer.',
   ADMIN_TRANSFER_ALREADY_PENDING = 'Administrator rights transfer is already pending.',
+  ADMIN_TRANSFER_NOT_PENDING = 'No administrator rights transfer is pending.',
+  ADMIN_TRANSFER_CANNOT_CANCEL = 'Administrator rights transfer can no longer be cancelled.',
+  ONLY_TRANSFER_INITIATOR_CAN_CANCEL = 'Only the administrator who initiated the transfer can cancel it.',
   CURRENT_USER_BLOCKED = 'Current user is blocked.',
   TARGET_USER_BLOCKED = 'Target user is blocked.',
   TARGET_USER_ALREADY_ADMINISTRATOR = 'Target user is already an administrator.',
