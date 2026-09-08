f19d857b83cdc1eaf9c229609c25924129d74a86
Aleksandr Petrov <37398682+aleksr777@users.noreply.github.com>
2026-09-08 17:26:16 +0500

    Stop using ADMIN_EMAIL at runtime

--- src/common/env-service/env.service.ts
+++ src/common/env-service/env.service.ts
@@ -56,13 +56,5 @@ export class EnvService {
       this.errorsService.default(null, msg);
       process.exit(1);
     }
-    const smtpFrom = this.get('SMTP_FROM');
-    const adminEmail = this.get('ADMIN_EMAIL');
-    if (smtpFrom === adminEmail) {
-      const msg =
-        'Env validation error: SMTP_FROM and ADMIN_EMAIL must not be the same.';
-      this.errorsService.default(null, msg);
-      process.exit(1);
-    }
   }
 }
