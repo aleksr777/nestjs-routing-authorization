18fbd3da4836946c9539693d6f05a5091e10abac
Aleksandr Petrov <37398682+aleksr777@users.noreply.github.com>
2026-09-10 12:00:12 +0500

    Require admin password for user blocking

--- src/admin/dto/block-user.dto.ts
+++ src/admin/dto/block-user.dto.ts
@@ -1,8 +1,12 @@
-import { MaxLength, IsString, IsOptional } from 'class-validator';
+import { IsOptional, IsString, Length, MaxLength } from 'class-validator';
 
 export class BlockUserDto {
   @IsOptional()
   @IsString()
   @MaxLength(255)
   blocked_reason?: string;
+
+  @IsString()
+  @Length(8, 100)
+  password!: string;
 }
