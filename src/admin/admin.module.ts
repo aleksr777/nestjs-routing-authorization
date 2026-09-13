ef0e119419792ef27c18700697cabfac62fa92a9
Aleksandr Petrov <37398682+aleksr777@users.noreply.github.com>
2026-09-13 22:41:29 +0500

    Remove admin transfer service and controller

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '../auth/auth.module';
 import { AdminService } from './admin.service';
-import { AdminTransferService } from './transfer-rights.service';
 import { AdminController } from './admin.controller';
-import { AdminTransferSecureController } from './transfer-rights.controller';
 import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), AuthModule],
-  controllers: [AdminController, AdminTransferSecureController],
-  providers: [AdminService, AdminTransferService],
+  controllers: [AdminController],
+  providers: [AdminService],
 })
 export class AdminModule {}
