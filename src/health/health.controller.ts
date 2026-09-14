ba4a2864323202b59687fa44585426d4d671b85f
Aleksandr Petrov <37398682+aleksr777@users.noreply.github.com>
2026-09-14 17:15:22 +0500

    Format health controller

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,8 +1,4 @@
-import {
-  Controller,
-  Get,
-  ServiceUnavailableException,
-} from '@nestjs/common';
+import { Controller, Get, ServiceUnavailableException } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { RedisService } from '../common/redis-service/redis.service';
 
